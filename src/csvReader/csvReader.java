2770a4167b0a09643a07e68a0de75b2242626130
Isaac Prior <isaacprior5@gmail.com>
2022-03-31 11:00:42 +1100

    Added file reading functionality

--- src/csvReader/csvReader.java
+++ src/csvReader/csvReader.java
@@ -1,9 +1,43 @@
 package csvReader;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class csvReader {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Initial Values
+		String csvFile = "testCSV.csv";
+		
+		Object[] records = new Object[6];
+		
+		//File Reader
+		try {
+			Scanner sc = new Scanner(new File(csvFile));
+			//sc.useDelimiter(",");
+			
+			//Skipping Header
+			sc.nextLine();
+		
+			while (sc.hasNextLine()) {
+				//Adding current line of CSV to array
+				records = sc.nextLine().split(",");
+				
+				//Temporary print for verification
+				System.out.println(Arrays.toString(records));
+
+			}
+			
+			
+			
+			sc.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
 
 	}
 
